9df642279d0b607f72f6d79dc63685b8f9f9daab
K0x86 <101434877+Kait84@users.noreply.github.com>
2024-04-10 23:38:03 +0200

    Delete Progr2/Praktikum/Praktikum-02-GdlgObjektorient/passwordmanager directory

--- Progr2/Praktikum/Praktikum-02-GdlgObjektorient/passwordmanager/src/main/java/PasswordEntry.java
+++
@@ -1,7 +0,0 @@
-public class PasswordEntry {
-    static final String website;
-    final String loginName;
-    final String passwordComplexity;
-    String password;
-    PasswordEntry nextEntry = null;
-}
