e4333049e609593808a7fe28cd029be3f57c1ebe
K0x86 <101434877+Kait84@users.noreply.github.com>
2024-05-23 01:03:02 +0200

    Delete Progr2/Praktikum/Praktikum-02-GdlgObjektorient/passwordmanager/src/main/java/testmain.java

--- Progr2/Praktikum/Praktikum-02-GdlgObjektorient/passwordmanager/src/main/java/testmain.java
+++
@@ -1,7 +0,0 @@
-public class testmain {
-    public static void main(String[] args){
-        System.out.prinltn(PasswordComplexity.MEDIUM.generatePassword().toString());
-
-        //aktueller Stand: PasswordStore: contains
-    }
-}
